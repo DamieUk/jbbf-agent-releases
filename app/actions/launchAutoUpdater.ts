cea4d697580eda166cf836889436fb5aba6904d7
Dmytro Poltavsky <dmytro@jbbf.ch>
2021-03-11 15:38:44 +0200

    fix for logger

--- app/actions/launchAutoUpdater.ts
+++ app/actions/launchAutoUpdater.ts
@@ -1,6 +1,7 @@
 // @ts-ignore
 import AutoUpdater from 'auto-updater';
 import {IAnyFunc} from "global-shapes";
+import logger from "../utils/logger";
 
 const initUpdater = (): IAnyFunc => {
   const autoupdater = new AutoUpdater({
@@ -13,36 +14,36 @@ const initUpdater = (): IAnyFunc => {
   });
 
   autoupdater.on('git-clone', () => {
-    console.log("You have a clone of the repository. Use 'git pull' to be up-to-date");
+    logger.info("You have a clone of the repository. Use 'git pull' to be up-to-date");
   });
 
   autoupdater.on('check.up-to-date', (v: string) => {
-    console.info("You have the latest version: " + v);
+    logger.info("You have the latest version: " + v);
   });
 
   autoupdater.on('check.out-dated', (v_old: string, v : string) => {
-    console.warn("Your version is outdated. " + v_old + " of " + v);
+    logger.warn("Your version is outdated. " + v_old + " of " + v);
     autoupdater.fire('download-update'); // If autoupdate: false, you'll have to do this manually.
     // Maybe ask if the'd like to download the update.
   });
 
   autoupdater.on('update.downloaded', () => {
-    console.log("Update downloaded and ready for install");
+    logger.info("Update downloaded and ready for install");
     autoupdater.fire('extract'); // If autoupdate: false, you'll have to do this manually.
   });
 
   autoupdater.on('update.not-installed', () => {
-    console.log("The Update was already in your folder! It's read for install");
+    logger.info("The Update was already in your folder! It's read for install");
     autoupdater.fire('extract'); // If autoupdate: false, you'll have to do this manually.
   });
 
   autoupdater.on('update.extracted', () => {
-    console.log("Update extracted successfully!");
-    console.warn("RESTART THE APP!");
+    logger.info("Update extracted successfully!");
+    logger.warn("RESTART THE APP!");
   });
 
   autoupdater.on('download.start', (name: string) => {
-    console.log("Starting downloading: " + name);
+    logger.info("Starting downloading: " + name);
   });
 
   autoupdater.on('download.progress', (name: string, perc: string) => {
@@ -50,19 +51,19 @@ const initUpdater = (): IAnyFunc => {
   });
 
   autoupdater.on('download.end', (name: string) => {
-    console.log("Downloaded " + name);
+    logger.info("Downloaded " + name);
   });
 
   autoupdater.on('download.error', (err: any) => {
-    console.error("Error when downloading: " + err);
+    logger.error("Error when downloading: " + err);
   });
 
   autoupdater.on('end', () => {
-    console.log("The app is ready to");
+    logger.info("The app is ready to");
   });
 
   autoupdater.on('error', (name: string, e: any) => {
-    console.error(name, e);
+    logger.error(name, e);
   });
 
   return (): IAnyFunc => {
